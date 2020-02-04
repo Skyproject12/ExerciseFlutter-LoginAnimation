a5649044f6bb2749df4c375378fc728186e10e95
Kadek Sumendra <sumendrakadek108@gmail.com>
2020-02-04 18:13:49 +0700

    fix animation

--- lib/CustomGestureDetector.dart
+++ lib/CustomGestureDetector.dart
@@ -1,57 +1,52 @@
-import 'package:flutter/material.dart'; 
-class CustomGestureDetector extends StatelessWidget { 
-  static const int AXIS_X =0; 
-  static const int AXIS_Y=1; 
-  static const int AXIS_BOTH =2; 
-  final int axis; 
-  final Widget child; 
-  final double velocity; 
-  final Function onSwipeUp; 
-  final Function onSwipeDown; 
-  final Function onSwipeLeft; 
-  final Function onSwipeRight; 
-  CustomGestureDetector({
-    @required this.child
-    , @required this.velocity
-    , @required this.onSwipeUp
-    , @required this.onSwipeRight
-    , @required this.onSwipeLeft
-    , @required this.onSwipeDown
-    , @required this.axis
-    });
+import 'package:flutter/cupertino.dart';
+
+class CustomGestureDetector extends StatelessWidget{
+
+  static const int AXIS_X = 0;
+  static const int AXIS_Y = 1;
+  static const int AXIS_BOTH = 2;
+
+  final int axis;
+  final Widget child;
+  final double velocity;
+  final Function onSwipeUp;
+  final Function onSwipeDown;
+  final Function onSwipeLeft;
+  final Function onSwipeRight;
+
+  CustomGestureDetector({@required this.child,@required this.velocity,this.onSwipeLeft,this.onSwipeRight,this.onSwipeUp,this.onSwipeDown,@required this.axis});
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector( 
-      onPanEnd: (details){ 
-        Offset v = details.velocity.pixelsPerSecond; 
-        try{ 
-          if((axis == AXIS_Y) || axis == AXIS_BOTH){ 
-            if(v.dy>velocity){ 
-              onSwipeDown();
-            } 
-            else if( 
-              v.dy<-velocity
-            ) { 
-              onSwipeUp();
-            } 
-          }
-            if((axis == AXIS_X) || axis == AXIS_BOTH){ 
-              if(v.dx>velocity){ 
+    // TODO: implement build
+    return GestureDetector(
+        onPanEnd: (details){
+          // menhgambil pexel persecond 
+           Offset v = details.velocity.pixelsPerSecond;
+
+          try { 
+            // mengecek axis 
+            if((axis == AXIS_Y) || axis == AXIS_BOTH){
+              if(v.dy>velocity){
+                onSwipeDown();
+              } else if(v.dy<-velocity){
+                onSwipeUp();
+              }
+            }
+            // melakukan pengecekan axis untuk mengatur status swife 
+            if((axis == AXIS_X )  || axis == AXIS_BOTH){
+              if(v.dx>velocity){
                 onSwipeRight();
-              } 
-              else if(v.dx<-velocity){ 
+              } else if(v.dx<-velocity) {
                 onSwipeLeft();
               }
-            } 
-        } 
-        catch(e){ 
-          debugPrintStack( 
-            label: "***** Function Not Define ******", 
-            //stackTrace: StackTrace.fromString("Please define the function for give constant")
-          );  
-        }
-      }, 
-      child: child
-    );
+            }
+          } catch (e){
+           
+          }
+
+        },
+        child: child);
   }
+
 }
